99af884e61edb724bb50075a84bda7976336eddb
TrangDC <dinhcongtrang99@gmail.com>
2024-02-01 10:22:27 +0700

    done basic update method. no yup or formik

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,26 +1,66 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link, useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import { motion } from 'framer-motion';
+import { useFormik } from "formik";
 
 function Edit() {
+    const [user, setUser] = useState({
+        name: {
+            firstname: '',
+            lastname: '',
+        },
+        email: '',
+        username: '',
+        phone: '',
+    });
 
-    const [user, setUser] = useState([]);
-
-    const {id} = useParams();
+    const { id } = useParams();
 
     useEffect(() => {
         const fetchUser = async () => {
             try {
                 const response = await axios.get(`http://localhost:3000/users/${id}`);
                 setUser(response.data);
-                console.log(response)
             } catch (err) {
                 console.log(err);
             }
-        }
+        };
         fetchUser();
     }, [id]);
+
+    const handleName = (event) => {
+        const { name, value } = event.target;
+
+        // Tách tên trường và tên con trường
+        const [parent, child] = name.split('.');
+
+        setUser(prevUser => ({
+            ...prevUser,
+            [parent]: {
+                ...prevUser[parent],
+                [child]: value,
+            },
+        }));
+    };
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setUser({...user, [name]: value})
+    }
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.put('http://localhost:3000/users/' + id, user)
+            .then(res => {
+                console.log(res);
+                navigate("/users")
+            })
+            .catch(err => console.log(err))
+    }
+
 
     return (
         <motion.div
@@ -31,36 +71,41 @@ function Edit() {
             transition={{ duration: 0.5 }}
         >
             <div className='w-50 border bg-secondary text-white p-5'>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h2>Update a user</h2>
                     <div>
                         <label htmlFor="name.firstname">First Name:</label>
                         <input type="text" name='name.firstname' className='form-control'
                                value={user.name && `${user.name.firstname}`}
+                               onChange={handleName}
                         />
                     </div>
                     <div>
                         <label htmlFor="name.lastname">Last Name:</label>
                         <input type="text" name='name.lastname' className='form-control'
                                value={user.name && `${user.name.lastname}`}
+                               onChange={handleName}
                         />
                     </div>
                     <div>
                         <label htmlFor="username">Username:</label>
                         <input type="text" name='username' className='form-control'
                                value={user.username}
+                               onChange={handleChange}
                         />
                     </div>
                     <div>
                         <label htmlFor="email">Email:</label>
                         <input type="email" name='email' className='form-control'
                                value={user.email}
+                               onChange={handleChange}
                         />
                     </div>
                     <div>
                         <label htmlFor="phone">Phone:</label>
                         <input type="number" name='phone' className='form-control'
                                value={user.phone}
+                               onChange={handleChange}
                         />
                     </div>
                     <br/>
